b6faff29335f9a17bb43bcdd590d12545364f012
Hemlocked <phantomofrotten@gmail.com>
2019-11-16 17:06:59 +0900

    successful in catching api json

--- ui/src/components/Index/index.tsx
+++ ui/src/components/Index/index.tsx
@@ -3,22 +3,22 @@ import React, { FC } from 'react';
 import axios from 'axios';
 
 export default class Index extends React.Component {
-    // state = {
-    //   persons: []
-    // }
+    state = {
+      persons: []
+    }
 
-    // componentDidMount() {
-    //   axios.get(`https://jsonplaceholder.typicode.com/users`)
-    //     .then(res => {
-    //       const persons = res.data;
-    //       this.setState({ persons });
-    //     })
-    // }
+    componentDidMount() {
+      axios.get(`http://localhost:5000/api/v1`)
+        .then(res => {
+          const persons = res.data;
+          this.setState({ persons });
+        })
+    }
 
     render() {
       return (
         <ul>
-          {/* { this.state.persons.map(person => <li>{person.name}</li>)} */}
+          { this.state.persons }
           <h1>Here is index</h1>
         </ul>
       )
