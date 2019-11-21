f651f63ae19f0dbd95ead15bd1080242870d7bb3
Hemlocked <phantomofrotten@gmail.com>
2019-11-21 10:50:27 +0900

    add metamask component 🦊

---
+++ react/src/components/Metamask/index.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from 'react';
+
+export default class Metamask extends React.Component {
+    render() {
+        return (
+            <h1>Here is metamask</h1>
+        )
+    }
+};
