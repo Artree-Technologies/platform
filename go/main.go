665f67edbf56cfa1211701dc2da6453a14ef6149
Hemlocked <phantomofrotten@gmail.com>
2019-11-21 10:40:02 +0900

    👋

--- go/main.go
+++ go/main.go
@@ -1,10 +1,27 @@
 package main
 
 import (
-	"./server"
+	"log"
+	"net/http"
+
+	"github.com/ant0ine/go-json-rest/rest"
 )
 
 func main() {
-	var port = "5000"
-	server.Start(port)
+	api := rest.NewApi()
+	api.Use(rest.DefaultDevStack...)
+	router, err := rest.MakeRouter(
+		rest.Get("/api/v1", GetMessage),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	api.SetApp(router)
+	log.Fatal(http.ListenAndServe(":5000", api.MakeHandler()))
+}
+
+func GetMessage(w rest.ResponseWriter, r *rest.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+	w.WriteJson(map[string]string{"message": "hello world!"})
 }
